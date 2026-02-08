71a3daf4310848ef457c1cbce5b8f52e14ee4534
Joey Stanford <nv0n@denvermesh.org>
2026-02-07 22:06:27 -0700

    Remove sudo from topgrade command

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 sudo docker-compose -f /home/andrew/docker/docker-compose.denmesh.yml stop
-sudo topgrade --disable firmware
+topgrade --disable firmware
 sudo docker-compose -f /home/andrew/docker/docker-compose.denmesh.yml pull
 sudo docker-compose -f /home/andrew/docker/docker-compose.denmesh.yml up -d
 sudo docker system prune -af
