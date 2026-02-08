acc4d85ac16218bf3ce27d2c2bbf27b35d710db1
Joey Stanford <nv0n@denvermesh.org>
2026-02-07 22:09:54 -0700

    Enhance update script with service stop and cleanup
    Updated the update.sh script to include system maintenance and cleanup steps.

--- update.sh
+++ update.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/bash
+
+# 1. Stop the services to release any locks on image layers
 sudo docker-compose -f /home/andrew/docker/docker-compose.denmesh.yml stop
+
+# 2. Prune BEFORE the pull to clear out old images and cache
+# This ensures you have room for the new incoming data
+sudo docker system prune -af
+
+# 3. System maintenance
 topgrade --disable firmware
+sudo dnf autoremove -y
+
+# 4. Pull and Restart
 sudo docker-compose -f /home/andrew/docker/docker-compose.denmesh.yml pull
 sudo docker-compose -f /home/andrew/docker/docker-compose.denmesh.yml up -d
-sudo docker system prune -af
-sudo dnf autoremove
